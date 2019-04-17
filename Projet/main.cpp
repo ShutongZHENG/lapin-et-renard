05ba269baec0d023e029790dd21988d96dff98b0
Shutong ZHENG <shutong.zheng@gmail.com>
2019-04-17 20:03:50 +0200

    Initial Commit

---
+++ Projet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Projet
+//
+//  Created by 郑疏楠 on 17/04/2019.
+//  Copyright © 2019 郑疏楠. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
