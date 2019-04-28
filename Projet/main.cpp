f1e6eb680455cc2edb40460b3b74d65097dc134d
Shutong ZHENG <shutong.zheng@gmail.com>
2019-04-28 17:11:14 +0200

    3th

--- Projet/main.cpp
+++ Projet/main.cpp
@@ -17,12 +17,13 @@ void AfficheGraphie(grille g){
         for (int j=0; j<20; j++) {
             switch (g.A[i][j].Ep) {
                 case 0:
-                    fenetre.draw_rectangle ( {10*i, 10*i}, 9, 9, color::blanc );
+                    fenetre.draw_rectangle ( {10*i, 10*j}, 9, 9, color::blanc );
                     break;
                 case 1:
-                    fenetre.draw_rectangle ( {10*i, 10*i}, 9, 9, color::red );
+                    fenetre.draw_rectangle ( {10*i, 10*j}, 9, 9, color::red );
+                    break;
                 case 2:
-                    fenetre.draw_rectangle ( {10*i, 10*i}, 9, 9, color::blue );
+                    fenetre.draw_rectangle ( {10*i, 10*j}, 9, 9, color::blue );
                     break;
                 default:
                     break;
